73a845c87167ecf887f71058579b8fd6043e61d4
Altun2002 <altunpaj@gmail.com>
2025-04-21 20:47:40 +0200

    dd

---
+++ Build/_deps/assimp-build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/altun/Documents/eduEngine/Build/_deps/assimp-src/contrib/zlib
+# Build directory: C:/Users/altun/Documents/eduEngine/Build/_deps/assimp-build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
